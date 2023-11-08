a6b1324cb6ba9aae99211cb4e7ea67176cf0db6d
Jonas Lehmann <lehmann.jonas@web.de>
2023-11-08 02:57:31 +0100

    implemented the lattice creation with transitive reduction

---
+++ bundles/org.kit.kastel.sdq.coupling.alignment.accessanalysis2joana/src/edu/kit/kastel/sdq/coupling/alignment/accessanalysis2joana/modelgenerators/iterative/transitivereduction/ValidRelation.java
@@ -0,0 +1,22 @@
+package edu.kit.kastel.sdq.coupling.alignment.accessanalysis2joana.modelgenerators.iterative.transitivereduction;
+
+import java.util.Set;
+
+public class ValidRelation<T> {
+
+	Set<T> fromCombinedLevel;
+	Set<T> toCombinedLevel;
+	
+	public ValidRelation(Set<T> fromCombinedLevel, Set<T> toCombinedLevel) {
+		this.fromCombinedLevel = fromCombinedLevel;
+		this.toCombinedLevel = toCombinedLevel;
+	}
+	
+	public Set<T> getFrom() {
+		return this.fromCombinedLevel;
+	}
+	
+	public Set<T> getTo() {
+		return this.toCombinedLevel;
+	}
+}
